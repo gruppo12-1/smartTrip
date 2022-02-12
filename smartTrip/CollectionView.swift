1003062e138a086c4f665f852d3e52eb5307aee2
salvatoreA0 <92514976+salvatoreA0@users.noreply.github.com>
2022-02-12 10:11:25 +0100

    Add files via upload

---
+++ smartTrip/CollectionView.swift
@@ -0,0 +1,20 @@
+//
+//  CollectionView.swift
+//  smartTrip
+//
+//  Created by Salvatore Apicella on 12/02/22.
+//
+
+import SwiftUI
+
+struct CollectionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CollectionView_Previews: PreviewProvider {
+    static var previews: some View {
+        CollectionView()
+    }
+}
