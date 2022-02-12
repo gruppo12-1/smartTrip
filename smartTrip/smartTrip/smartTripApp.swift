7671c5468e74e82928b9c4efe70bdd001f5a5052
salvatoreA0 <92514976+salvatoreA0@users.noreply.github.com>
2022-02-12 10:02:18 +0100

    Add files via upload

---
+++ smartTrip/smartTrip/smartTripApp.swift
@@ -0,0 +1,17 @@
+//
+//  smartTripApp.swift
+//  smartTrip
+//
+//  Created by Salvatore Apicella on 12/02/22.
+//
+
+import SwiftUI
+
+@main
+struct smartTripApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
