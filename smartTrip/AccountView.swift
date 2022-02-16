a245b5d4b52ccd829763b1c08fcf370eb0614ea0
my-rice <dvdrsi2017@gmail.com>
2022-02-16 10:20:34 +0100

    AccountView: Bozza

--- smartTrip/AccountView.swift
+++ smartTrip/AccountView.swift
@@ -9,7 +9,62 @@ import SwiftUI
 
 struct AccountView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        
+        NavigationView {
+    
+            ZStack{
+                VStack(alignment: .center){
+                    Image("PicProva")
+                        .resizable()
+                        .frame(width: 200, height: 200)
+                        .clipShape(Circle())
+                    
+                    Form{
+                        Section{
+                            VStack{
+                                HStack{
+                                    Button{} label: {
+                                        Label("settings",systemImage: "gear")
+                                    }
+                                    .padding()
+                                    .buttonStyle(.bordered)
+                                    
+                                    Spacer(minLength: 0)
+                                    
+                                    Button{} label: {
+                                        Label("share",systemImage: "square.and.arrow.up")
+                                    }
+                                    .padding()
+                                    .buttonStyle(.bordered)
+                                    
+                                }
+                            }
+                        }
+                        
+                        Section(){
+                            Text("Nickname")
+                            Text("Oggetti collezionati")
+                            Text("OtherInfo")
+                            
+                        }
+                        Section(){
+                            Text("Otherthings")
+                        }
+                    }
+                                        
+                }
+                
+            }
+            .navigationTitle("Account Setting")
+            .navigationBarItems(leading: Button(action:{
+
+                
+            }) {
+                Text("Cancel")
+            })
+        }
+        
     }
 }
 
